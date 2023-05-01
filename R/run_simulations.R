134d68d3cf38a70e7a20a24cfe639a0e74299f8f
Marlene Stratmann <marlene.stratmann@kau.se>
2023-05-01 13:07:17 +0200

    Initial commit

---
+++ R/run_simulations.R
@@ -0,0 +1,71 @@
+#' Power calculation with Monte Carlo simulation
+#'
+#' This function helps you calculate power for regressions
+#' using Monte Carlo simulation with several variables
+#' that potentially have interactions.
+#'
+#' @param name description
+#'
+#' @return description
+#'
+#' @importFrom data.table .N
+#' @export run_simulations
+
+run_simulations <- function(n_obs            = 10000,
+                            n_simulations    = 1000,
+                            n_parallel_cores = 5,
+                            p_preterm        = list(very     = 0.002,
+                                                    moderate = 0.05,
+                                                    late     = 0.1),
+                            p_autism         = 0.01,
+                            p_geriatric_preg = 1/8) {
+
+  # Defining variables used later by data.table
+  power <- p_value <- term <- NULL
+
+  # Set up paralisation
+  cl <- parallel::makeCluster(n_parallel_cores)
+
+  parallel::clusterExport(cl, c("simulate", "n_obs", "p_preterm",
+                                "p_autism", "p_geriatric_preg"),
+                          envir = rlang::current_env())
+
+  # Run simulaiton
+  simulations <- parallel::parLapply(cl, 1:n_simulations, function(i) {
+
+    set.seed(i * 7)
+
+    fit <- simulate(n_obs, p_preterm, p_autism, p_geriatric_preg)
+
+    fit$iteration <- i
+
+    return(fit)
+
+  })
+
+  # Stop cluster
+  parallel::stopCluster(cl)
+
+  # Create dataset of simulations
+  simulations <- data.table::rbindlist(simulations)
+
+  cat("Simulated", n_simulations, "datasets with", n_obs, "observation\n")
+  cat("using the following parameters:\n")
+  cat("--------------------------------------\n")
+  cat("Preterm prevalences\n")
+  cat("   very:    "                   , p_preterm[["very"]]    , "\n")
+  cat("   moderate:"                   , p_preterm[["moderate"]], "\n")
+  cat("   late:    "                   , p_preterm[["late"]]    , "\n")
+  cat("Autism prevalence:             ", p_autism               , "\n")
+  cat("Geriatric pregnancy prevalence:", p_geriatric_preg       , "\n")
+  cat("--------------------------------------\n")
+
+
+  cat("\nSimulation results:\n")
+  # Calculate power
+  out <- simulations[, list(power = sum(p_value < 0.05) / .N),
+                     by = term]
+
+  return(out)
+
+}
---
+++ R/simulate.R
@@ -0,0 +1,33 @@
+#' Power calculation with Monte Carlo simulation
+#'
+#' Helper function for `run_simulations()`.
+#' For more information see parameter definition there.
+
+
+simulate <- function(n_obs, p_preterm, p_autism, p_geriatric_preg) {
+
+  dta <- data.frame(id = 1:n_obs)
+
+  dta$geriatric_preg <- stats::rbinom(n_obs, 1, p_geriatric_preg)
+
+  dta$preterm <- pmax(0,
+                      stats::rbinom(n_obs, 1, p_preterm[["very"]])     * 1,
+                      stats::rbinom(n_obs, 1, p_preterm[["moderate"]]) * 2,
+                      stats::rbinom(n_obs, 1, p_preterm[["late"]])     * 3)
+
+  dta$autism <- stats::rbinom(n_obs, 1, p_autism
+                              + dta$geriatric_preg  * 0.04
+                              + I(dta$preterm == 1) * 0.10
+                              + I(dta$preterm == 2) * 0.05
+                              + I(dta$preterm == 3) * 0.025
+                              + dta$geriatric_preg * I(dta$preterm > 0) * 0.05)
+
+  model <- stats::glm(autism ~ geriatric_preg
+                      + factor(preterm)
+                      + I(dta$preterm > 0):geriatric_preg,
+                      data = dta,
+                      family = "binomial()")
+
+  broom::tidy(model)
+
+}
